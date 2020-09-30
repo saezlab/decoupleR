278067aae6c9d267b3c1ec95c395af2f7d20a2a1
Jesús Vélez Santiago <jvelez@lcg.unam.mx>
2020-09-29 20:39:04 -0500

    Add tests for run_scira() function

--- inst/testdata/generate_expected_output.R
+++ inst/testdata/generate_expected_output.R
@@ -27,3 +27,16 @@ run_viper(emat, progeny_genesets, gs_resource="progeny", tidy=TRUE) %>%
 
 run_viper(emat, progeny_genesets, options = list(minsize=4), gs_resource="progeny", tidy=FALSE) %>%
   saveRDS("inst/testdata/outputs/viper/output-viper_progeny_minsize4.rds")
+
+#----- run_scira() -------------------------------------------------------------
+
+out_scira_dir <- file.path("inst", "testdata", "outputs", "scira")
+
+run_scira(emat, dorothea_genesets) %>%
+  saveRDS(file.path(out_scira_dir, "output-scira_dorothea_default.rds"))
+
+run_scira(emat, dorothea_genesets, tf, target, mor) %>%
+  saveRDS(file.path(out_scira_dir, "output-scira_dorothea_tidy-evaluation.rds"))
+
+run_scira(emat, dorothea_genesets, .sparse = TRUE) %>%
+  saveRDS(file.path(out_scira_dir, "output-scira_dorothea_sparse-background-calculation.rds"))
---
+++ tests/testthat/test-scira.R
@@ -0,0 +1,46 @@
+library(decoupleR)
+
+# Data to run -------------------------------------------------------------
+
+emat <- readRDS(
+  system.file("testdata/inputs", "input-expr_matrix.rds", package = "decoupleR")
+)
+dorothea_genesets <- readRDS(
+  system.file("testdata/inputs", "input-dorothea_genesets.rds", package = "decoupleR")
+)
+progeny_genesets <- readRDS(
+  system.file("testdata/inputs", "input-progeny_genesets.rds", package = "decoupleR")
+)
+
+# Test for run_scira function ---------------------------------------------
+
+test_that("test run_scira with dorothea gene sets", {
+  res1 <- run_scira(emat, dorothea_genesets)
+  exp1 <- readRDS(
+    system.file("testdata/outputs/scira/", "output-scira_dorothea_default.rds",
+                package = "decoupleR"
+    )
+  )
+
+  res2 <- run_scira(emat, dorothea_genesets, tf, target, mor)
+  exp2 <- readRDS(
+    system.file("testdata/outputs/scira/", "output-scira_dorothea_tidy-evaluation.rds",
+                package = "decoupleR"
+    )
+  )
+
+  res3 <- run_scira(emat, dorothea_genesets, .sparse = TRUE)
+  exp3 <- readRDS(
+    system.file("testdata/outputs/scira/", "output-scira_dorothea_sparse-background-calculation.rds",
+                package = "decoupleR"
+    )
+  )
+
+  expect_equal(res1, exp1)
+  expect_equal(res2, exp2)
+  expect_equal(res3, exp3)
+})
+
+# test_that("test run_scira with progeny gene sets", {
+#
+# })

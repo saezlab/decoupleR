e409f096e028e5bbabbc0f9a6d555b62addac88c
Jesús Vélez Santiago <jvelez@lcg.unam.mx>
2020-10-09 20:31:20 -0500

    Add complete run_mean() method. Internally it also runs run_normilized_mean().

--- R/mean.R
+++ R/mean.R
@@ -12,7 +12,6 @@
 #' @param .likelihood Column with edge likelihood.
 #' @param minsize How many output edges a source node must have to be included
 #'  in the analysis?
-#' @param mixed If TRUE permutations are performed only within the positive and negative values.
 #' @param times How many permutations to do?
 #' @param seed A single value, interpreted as an integer, or NULL for random number generation.
 #' @param sparse Should the matrices used for the calculation be sparse?
@@ -26,6 +25,7 @@
 #'  }
 #'
 #' @export
+#' @import dplyr
 run_mean <- function(mat,
                      network,
                      .source = .data$tf,
@@ -33,10 +33,9 @@ run_mean <- function(mat,
                      .mor = .data$mor,
                      .likelihood = .data$likelihood,
                      minsize = 1,
-                     mixed = FALSE,
                      times = 2,
                      seed = 42,
-                     sparse = FALSE) {
+                     sparse = TRUE) {
 
   # Before to start ---------------------------------------------------------
   if (times < 2) {
@@ -49,6 +48,7 @@ run_mean <- function(mat,
   # Preprocessing -----------------------------------------------------------
 
   network <- network %>%
+    filter(.data$target %in% rownames(mat)) %>%
     add_count(.data$tf, name = "out_degree") %>%
     filter(.data$out_degree >= minsize) %>%
     add_count(.data$tf, wt = .data$likelihood, name = "contribution") %>%
@@ -60,19 +60,85 @@ run_mean <- function(mat,
     pull(.data$tf) %>%
     unique()
 
+  shared_targets <- network %>%
+    pull(.data$target) %>%
+    unique()
+
   targets <- rownames(mat)
   samples <- colnames(mat)
 
-  # Expands the target profile to encompass all targets in the
-  # expression matrix for each source.
-  network <- network %>%
-    get_profile_of(
-      sources = list(tf = tfs, target = targets),
+  # Extract matrix of weights
+  weight_mat <- network %>%
+    pivot_wider_profile(
+      .data$tf,
+      .data$target,
+      .data$weight,
+      to_matrix = TRUE,
+      to_sparse = sparse,
       values_fill = 0
     )
 
-  weight_mat <- network %>%
-    pivot_wider_profile(.data$tf, .data$target, .data$weight, to_matrix = TRUE, to_sparse = sparse)
-
   # Analysis ----------------------------------------------------------------
+
+  mean_run <- partial(
+    .mean_run,
+    .mat = mat,
+    .weight_mat = weight_mat,
+    .shared_targets = shared_targets
+  )
+
+
+  set.seed(seed)
+  map_dfr(1:times, ~ mean_run(random = TRUE)) %>%
+    group_by(.data$tf, .data$sample) %>%
+    summarise(
+      null_distribution = list(.data$value),
+      null_mean = mean(.data$value),
+      null_sd = stats::sd(.data$value),
+      .groups = "drop"
+    ) %>%
+    left_join(y = mean_run(), by = c("tf", "sample")) %>%
+    mutate(
+      z_score = (.data$value - .data$null_mean) / .data$null_sd,
+      z_score = replace_na(.data$z_score, 0),
+      p_value = map2_dbl(
+        .x = .data$null_distribution,
+        .y = .data$value,
+        .f = ~ sum(abs(.x) > abs(.y)) / length(.x)
+      )
+    ) %>%
+    select(-contains("null")) %>%
+    rename(mean = .data$value, normalized_mean = .data$z_score) %>%
+    pivot_longer(
+      cols = c(.data$mean, .data$normalized_mean),
+      names_to = "statistic",
+      values_to = "score"
+    ) %>%
+    arrange(.data$statistic, .data$tf, .data$sample) %>%
+    select(.data$tf, .data$sample, .data$score, .data$statistic, .data$p_value)
+}
+
+# Helper functions --------------------------------------------------------
+
+.mean_map_model_data <- function(.mat, .shared_targets, random = FALSE) {
+  if (random) {
+    .mat[sample(nrow(.mat)), ] %>%
+      `rownames<-`(rownames(.mat)) %>%
+      .[.shared_targets, ]
+  } else {
+    .mat[.shared_targets, ]
+  }
+}
+
+.mean_evaluate_model <- function(.mat, .weight_mat) {
+  (.weight_mat %*% .mat) %>%
+    as.matrix() %>%
+    as.data.frame() %>%
+    rownames_to_column("tf") %>%
+    pivot_longer(-.data$tf, names_to = "sample")
+}
+
+.mean_run <- function(.mat, .weight_mat, .shared_targets, random = FALSE) {
+  .mean_map_model_data(.mat, .shared_targets, random = random) %>%
+    .mean_evaluate_model(.weight_mat)
 }

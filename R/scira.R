1c47d405e1df15394829e407620663aa79898c18
Jesús Vélez Santiago <jvelez@lcg.unam.mx>
2020-10-19 14:32:09 -0500

    Change paramaters to a generalized form. Update documentation.

--- R/scira.R
+++ R/scira.R
@@ -11,18 +11,18 @@
 #' a member of the TF regulon. TF activity is then defined as the t-statistic of
 #' this linear regression.
 #'
-#' @param emat A named expression matrix. E.g Genes in rows and samples in columns.
-#' @param regulons A data frame of regulons in table format.
-#' @param .source Column name in regulons with the factors to calculate the enrichment scores.
-#' @param .target Column name in regulons that relates the labels of the rows in the expression matrix.
-#' @param .target_profile Column name in regulons that contains the target profile. E.g. MoR.
+#' @param mat A named expression matrix. E.g Genes in rows and samples in columns.
+#' @param network Tibble or dataframe with edges and associated metadata.
+#' @param .source Column with source nodes.
+#' @param .target Column with target nodes.
+#' @param .target_profile Column name in network that contains the target profile. E.g. MoR.
 #' @param .sparse Logical value indicating if the generated profile matrix should be sparse.
 #'
 #' @return A long format tibble of the enrichment results for each set of genes
 #'  across the samples. Resulting tibble contains the following columns:
 #'  \enumerate{
-#'    \item \code{source} Resources from the \code{.source} column of the \code{regulons} data frame.
-#'    \item \code{condition} Samples representing each column of \code{emat}.
+#'    \item \code{source} Resources from the \code{.source} column of the \code{network} data frame.
+#'    \item \code{condition} Samples representing each column of \code{mat}.
 #'    \item \code{score} Regulatory activity of each resource for each condition.
 #'  }
 #' @export
@@ -32,8 +32,8 @@
 #' @import tibble
 #' @import tidyr
 #' @importFrom stats coef lm summary.lm
-run_scira <- function(emat,
-                      regulons,
+run_scira <- function(mat,
+                      network,
                       .source = .data$tf,
                       .target = .data$target,
                       .target_profile = .data$mor,
@@ -42,27 +42,27 @@ run_scira <- function(emat,
   # Preprocessing -----------------------------------------------------------
 
   # Convert to standard tibble: tf-target-mor.
-  regulons <- regulons %>%
+  network <- network %>%
     convert_to_scira({{ .source }}, {{ .target }}, {{ .target_profile }}, clean = TRUE)
 
   # Extract labels that will map to the expression and profile matrices
-  tfs <- regulons %>%
+  tfs <- network %>%
     pull(.data$tf) %>%
     unique()
 
-  conditions <- colnames(emat)
+  conditions <- colnames(mat)
 
   # Ensures column matching, expands the target profile to encompass all targets
   # in the expression matrix for each source, and converts the result to a matrix.
-  profile_mat <- regulons %>%
+  profile_mat <- network %>%
     get_profile_of(
-      sources = list(tf = tfs, target = rownames(emat)),
+      sources = list(tf = tfs, target = rownames(mat)),
       values_fill = list(mor = 0)
     ) %>%
     pivot_wider_profile(.data$tf, .data$target, .data$mor, to_matrix = TRUE, to_sparse = .sparse)
 
   # Model evaluation --------------------------------------------------------
-  .scira_analysis(emat, profile_mat)
+  .scira_analysis(mat, profile_mat)
 }
 
 # Helper functions ------------------------------------------------------
@@ -75,13 +75,13 @@ run_scira <- function(emat,
 #' @inherit run_mean return
 #' @keywords intern
 #' @noRd
-.scira_analysis <- function(emat, target_profile_mat) {
+.scira_analysis <- function(mat, target_profile_mat) {
 
   # Allocate the space for all combinations of sources and conditions
   # and evaluate the proposed model.
-  lift_dl(expand_grid)(list(tf = rownames(target_profile_mat), condition = colnames(emat))) %>%
+  lift_dl(expand_grid)(list(tf = rownames(target_profile_mat), condition = colnames(mat))) %>%
     rowwise() %>%
-    mutate(score = .scira_run(.data$tf, .data$condition, emat, target_profile_mat))
+    mutate(score = .scira_run(.data$tf, .data$condition, mat, target_profile_mat))
 }
 
 #' Wrapper to run scira one tf per sample at time
@@ -94,8 +94,8 @@ run_scira <- function(emat,
 #'
 #' @keywords internal
 #' @noRd
-.scira_run <- function(source, condition, emat, target_profile_mat) {
-  .scira_map_model_data(source, condition, emat, target_profile_mat) %>%
+.scira_run <- function(source, condition, mat, target_profile_mat) {
+  .scira_map_model_data(source, condition, mat, target_profile_mat) %>%
     .scira_evaluate_model()
 }
 
@@ -108,8 +108,8 @@ run_scira <- function(emat,
 #' @return Tibble with the data to use to evaluate the model.
 #' @keywords internal
 #' @noRd
-.scira_map_model_data <- function(source, condition, emat, target_profile_mat) {
-  tibble(expression = emat[, condition], mor = target_profile_mat[source, ])
+.scira_map_model_data <- function(source, condition, mat, target_profile_mat) {
+  tibble(expression = mat[, condition], mor = target_profile_mat[source, ])
 }
 
 #' Evaluate model

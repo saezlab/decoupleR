aafbfa13099b670628eec1c39d7fb6d75b722cb4
Jesús Vélez Santiago <jvelez@lcg.unam.mx>
2020-10-24 16:58:53 -0500

    Update relative paths.

--- inst/testdata/generate_expected_output.R
+++ inst/testdata/generate_expected_output.R
@@ -1,98 +1,74 @@
-emat <- readRDS(
-  system.file("testdata/inputs", "input-expr_matrix.rds",
-    package = "decoupleR"
-  )
+library(decoupleR)
+
+# Directories -------------------------------------------------------------
+
+# Inputs
+input_dir <- system.file("testdata", "inputs", package = "decoupleR")
+
+# Outputs
+output_dir <- file.path("inst", "testdata", "outputs")
+
+out <- list(
+  mean = file.path(output_dir, "mean"),
+  pscira = file.path(output_dir, "pscira"),
+  scira = file.path(output_dir, "scira"),
+  viper = file.path(output_dir, "viper")
 )
-dorothea_genesets <- readRDS(
-  system.file("testdata/inputs", "input-dorothea_genesets.rds",
-    package = "decoupleR"
-  )
-)
-progeny_genesets <- readRDS(
-  system.file("testdata/inputs", "input-progeny_genesets.rds",
-    package = "decoupleR"
-  )
-)
+
+sapply(out, dir.create, showWarnings = TRUE)
+
+# Load data to generated outputs ------------------------------------------
+
+emat <- file.path(input_dir, "input-expr_matrix.rds") %>%
+  readRDS()
+
+dorothea_genesets <- file.path(input_dir, "input-dorothea_genesets.rds") %>%
+  readRDS()
+
+progeny_genesets <- file.path(input_dir, "input-progeny_genesets.rds") %>%
+  readRDS()
 
 #----- run_viper() -------------------------------------------------------------
 
 run_viper(emat, dorothea_genesets) %>%
-  saveRDS("inst/testdata/outputs/viper/output-viper_dorothea_default.rds")
+  saveRDS(file.path(out$viper, "output-viper_dorothea_default.rds"))
 
-run_viper(
-  emat,
-  dorothea_genesets,
-  tf,
-  target,
-  mor,
-  likelihood
+run_viper(emat, dorothea_genesets, tf, target, mor, likelihood) %>%
+  saveRDS(file.path(out$viper, "output-viper_dorothea_tidy-evaluation.rds"))
+
+run_viper(emat, dorothea_genesets, options = list(minsize = 4)
 ) %>%
-  saveRDS("inst/testdata/outputs/viper/output-viper_dorothea_tidy-evaluation.rds")
-
-run_viper(emat,
-  dorothea_genesets,
-  options = list(minsize = 4)
-) %>%
-  saveRDS("inst/testdata/outputs/viper/output-viper_dorothea_minsize4.rds")
-
-# run_viper(emat, progeny_genesets, gs_resource="progeny", tidy=FALSE) %>%
-#   saveRDS("inst/testdata/outputs/viper/output-viper_progeny_default.rds")
-#
-# run_viper(emat, progeny_genesets, gs_resource="progeny", tidy=TRUE) %>%
-#   saveRDS("inst/testdata/outputs/viper/output-viper_progeny_default_tidy.rds")
-#
-# run_viper(emat, progeny_genesets, options = list(minsize=4), gs_resource="progeny", tidy=FALSE) %>%
-#   saveRDS("inst/testdata/outputs/viper/output-viper_progeny_minsize4.rds")
+  saveRDS(file.path(out$viper, "output-viper_dorothea_minsize4.rds"))
 
 #----- run_scira() -------------------------------------------------------------
 
-out_scira_dir <- file.path("inst", "testdata", "outputs", "scira")
-
-# dorothea dataset
-
 run_scira(emat, dorothea_genesets) %>%
-  saveRDS(file.path(out_scira_dir, "output-scira_dorothea_default.rds"))
+  saveRDS(file.path(out$scira, "output-scira_dorothea_default.rds"))
 
 run_scira(emat, dorothea_genesets, tf, target, mor) %>%
-  saveRDS(file.path(out_scira_dir, "output-scira_dorothea_tidy-evaluation.rds"))
+  saveRDS(file.path(out$scira, "output-scira_dorothea_tidy-evaluation.rds"))
 
 run_scira(emat, dorothea_genesets, .sparse = TRUE) %>%
-  saveRDS(file.path(out_scira_dir, "output-scira_dorothea_sparse-background-calculation.rds"))
-
-# progeny dataset
-
-# run_scira(emat, progeny_genesets, gene, pathway, weight) %>%
-#   saveRDS(file.path(out_scira_dir, "output-scira_progeny_tidy-evaluation.rds"))
-#
-# run_scira(emat, progeny_genesets, gene, pathway, weight, .sparse = TRUE) %>%
-#   saveRDS(file.path(out_scira_dir, "output-scira_progeny_sparse-background-calculation.rds"))
+  saveRDS(file.path(out$scira, "output-scira_dorothea_sparse-background-calculation.rds"))
 
 #----- run_pscira() ------------------------------------------------------------
 
-out_pscira_dir <- file.path("inst", "testdata", "outputs", "pscira")
-
-# dorothea dataset
-
 run_pscira(emat, dorothea_genesets) %>%
-  saveRDS(file.path(out_pscira_dir, "output-pscira_dorothea_default.rds"))
+  saveRDS(file.path(out$pscira, "output-pscira_dorothea_default.rds"))
 
 run_pscira(emat, dorothea_genesets, tf, target, mor) %>%
-  saveRDS(file.path(out_pscira_dir, "output-pscira_dorothea_tidy-evaluation.rds"))
+  saveRDS(file.path(out$pscira, "output-pscira_dorothea_tidy-evaluation.rds"))
 
 run_pscira(emat, dorothea_genesets, .sparse = TRUE) %>%
-  saveRDS(file.path(out_pscira_dir, "output-pscira_dorothea_sparse-background-calculation.rds"))
+  saveRDS(file.path(out$pscira, "output-pscira_dorothea_sparse-background-calculation.rds"))
 
 #----- run_mean() ------------------------------------------------------------
 
-out_mean_dir <- file.path("inst", "testdata", "outputs", "mean")
-
-# dorothea dataset
-
 run_mean(emat, dorothea_genesets, .likelihood = NULL) %>%
-  saveRDS(file.path(out_mean_dir, "output-mean_dorothea_default.rds"))
+  saveRDS(file.path(out$mean, "output-mean_dorothea_default.rds"))
 
 run_mean(emat, dorothea_genesets, tf, target, mor, .likelihood = NULL) %>%
-  saveRDS(file.path(out_mean_dir, "output-mean_dorothea_tidy-evaluation.rds"))
+  saveRDS(file.path(out$mean, "output-mean_dorothea_tidy-evaluation.rds"))
 
 run_mean(emat, dorothea_genesets, sparse = TRUE, .likelihood = NULL) %>%
-  saveRDS(file.path(out_mean_dir, "output-mean_dorothea_sparse-background-calculation.rds"))
+  saveRDS(file.path(out$mean, "output-mean_dorothea_sparse-background-calculation.rds"))

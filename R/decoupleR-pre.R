af83eb658b4717032a615616e632f564b3ed284c
Pau Badia i Mompel <paubadiamompel@gmail.com>
2021-08-18 11:42:51 +0200

    Added intersect_regulon function

--- R/decoupleR-pre.R
+++ R/decoupleR-pre.R
@@ -22,3 +22,25 @@ filter_regulons <- function(network,
         filter(.data$n >= min_size, .data$n <= max_size) %>%
         select(-.data$n)
 }
+
+#' Intersect network target genes with expression matrix.
+#'
+#' Keep only edges which its target genes belong to the expression matrix.
+#'
+#' @inheritParams .decoupler_network_format
+#' @param .target Maximum number of targets allowed per regulon.
+#'
+#' @return Filtered tibble.
+#' @export
+#' @examples
+#' inputs_dir <- system.file("testdata", "inputs", package = "decoupleR")
+#' mat <- readRDS(file.path(inputs_dir, "input-expr_matrix.rds"))
+#' network <- readRDS(file.path(inputs_dir, "input-dorothea_genesets.rds"))
+#' filter_regulons(mat, network, target)
+intersect_regulons <- function(mat,
+                               network,
+                               target='target') {
+  targets <- rownames(mat)
+  network %>%
+    filter(target %in% targets)
+}

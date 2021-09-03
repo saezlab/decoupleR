986b84e76f30fa3e6875cb7d7f24f3e459f84d96
Pau Badia i Mompel <paubadiamompel@gmail.com>
2021-09-03 18:11:55 +0200

    Made AUCell use abs values

--- R/statistic-aucell.R
+++ R/statistic-aucell.R
@@ -6,8 +6,8 @@
 #'
 #' @inheritParams .decoupler_mat_format
 #' @inheritParams .decoupler_network_format
+#' @param aucMaxRank Threshold to calculate the AUC.
 #' @param nCores Number of cores to use for computation.
-#' @param weights Whether is network weighted
 #'
 #' @family decoupleR statistics
 #' @export
@@ -25,74 +25,38 @@ run_aucell <- function(mat,
                        network,
                        .source = .data$tf,
                        .target = .data$target,
-                       .mor = .data$mor,
-                       .likelihood = .data$likelihood,
-                       weights = FALSE,
+                       aucMaxRank = ceiling(0.05 * nrow(rankings)),
                        nCores = 1) {
+  # Before to start ---------------------------------------------------------
+
   # Check for NAs/Infs in mat
   check_nas_infs(mat)
-  if (-1 %in% network$mor | weights){
-    # Analysis ----------------------------------------------------------------
-    network %>%
-      convert_f_defaults(
-        tf = {{ .source }},
-        target = {{ .target }},
-        mor = {{ .mor }},
-        likelihood = {{ .likelihood }}
-      ) %>%
-      filter(target %in% rownames(mat)) %>% # Overlap between genes in the network and genes in the expression matrix
-      group_by(tf) %>%
-      group_modify(~ .one_TF_aucell_mor(.x, mat, nCores=nCores), .keep = TRUE) %>%
-      pivot_longer(-tf , names_to = "condition",  values_to = "score") %>%
-      add_column(statistic = "aucell", .before = 1) %>%
-      ungroup()
-  }
-  else {
-    # Before to start ---------------------------------------------------------
-    network <- network %>%
-      convert_to_aucell({{ .source }}, {{ .target }})
 
-    # Analysis ----------------------------------------------------------------
-    rankings <- exec(.fn = AUCell::AUCell_buildRankings,
-                     exprMat = mat,
-                     plotStats = FALSE,
-                     verbose = FALSE,
-                     nCores = nCores)
+  network <- network %>%
+    convert_to_aucell({{ .source }}, {{ .target }})
 
-    exec(.fn = AUCell::AUCell_calcAUC,
-         geneSets = network,
-         rankings = rankings,
-         verbose = FALSE,
-         nCores = nCores
-    ) %>%
-      .extract_assay_auc() %>%
-      as.data.frame() %>%
-      rownames_to_column("tf") %>%
-      pivot_longer(-tf ,names_to = "condition", values_to = "score") %>%
-      add_column(statistic = "aucell", .before = 1)
+  # Convert to absolute values
+  mat <- abs(mat)
 
-  }
-}
+  # Analysis ----------------------------------------------------------------
+  rankings <- exec(.fn = AUCell::AUCell_buildRankings,
+                   exprMat = mat,
+                   plotStats = FALSE,
+                   verbose = FALSE,
+                   nCores = nCores)
 
-.one_TF_aucell_mor <- function(network, mat, nCores){
-  # Multiply mat by (likelihood*mor)
-  mat[rownames(mat) %in% network$target,] <- (network$likelihood * network$mor) * mat[rownames(mat) %in% network$target,]
-
-  # Calculate rankings
-  .rankings <- AUCell::AUCell_buildRankings(mat, nCores=nCores, plotStats=FALSE, verbose=FALSE)
-
-  # Convert network into named list for the AUCell_calcAUC function
-  .network_aucell <- network %>%
-    group_by(tf) %>%
-    summarise(regulons = rlang::set_names(list(target), tf[1]),
-              .groups = "drop"
-    ) %>%
-    pull(regulons)
-
-  # Calculate AUC
-  AUCell::AUCell_calcAUC(.network_aucell, .rankings, nCores=nCores, verbose=FALSE) %>%
+  exec(.fn = AUCell::AUCell_calcAUC,
+       geneSets = network,
+       rankings = rankings,
+       verbose = FALSE,
+       aucMaxRank = aucMaxRank,
+       nCores = nCores
+  ) %>%
     .extract_assay_auc() %>%
-    as.data.frame()
+    as.data.frame() %>%
+    rownames_to_column("tf") %>%
+    pivot_longer(-tf ,names_to = "condition", values_to = "score") %>%
+    add_column(statistic = "aucell", .before = 1)
 
 }
 

9448c0f7444d0812c2ce588a93fd4afd04f325ff
adugourd <aurelien.dugourd@bioquant.uni-heidelberg.de>
2023-02-23 12:50:06 +0100

    added get_KSN_omnipath function

--- R/utils-omnipath.R
+++ R/utils-omnipath.R
@@ -148,3 +148,45 @@ get_progeny <- function(organism='human', top=500){
   return(p)
 }
 
+#' Omnipath KSN
+#'
+#' @description
+#' Generate a ready to use, curated Kinase/Substrate Network from omnipath
+#'
+#' @details
+#' Import PTM network from omnipath, then filter out anything that is not 
+#' phospho or dephosphorilation. Then format the columns for use with decoupleR
+#' functions.
+#' 
+#' @param ressources vector of character with ressource names if specified.
+#' 
+#' @return A `network` dataframe with 3 columns:
+#'  2. `source`: Source nodes of `network`.
+#'  3. `target`: Targets nodes of `network`.
+#'  4. `mor`: mode of regulation, indicates phosphorilations 
+#'  and dephsphorilations
+#' @export
+#'
+#' @examples
+#' KSN_omnipath <- get_KSN_omnipath()
+#' 
+get_KSN_omnipath <- function(resources = NULL){
+  KSN_omnipath <- as.data.frame(OmnipathR::import_omnipath_enzsub(resources = resources)) #resources = c("PhosphoSite","SIGNOR","KEA","MIMP","PhosphoNetworks","HPRD")
+  KSN_omnipath$psite <- paste(KSN_omnipath$substrate_genesymbol, paste(KSN_omnipath$residue_type, KSN_omnipath$residue_offset, sep = ""), sep = "_")
+  
+  KSN_omnipath <- KSN_omnipath[KSN_omnipath$modification %in% c("phosphorylation","dephosphorylation"),c(3,13,7)] 
+  KSN_omnipath$modification <- ifelse(KSN_omnipath$modification == "phosphorylation", 1, -1)
+  
+  names(KSN_omnipath) <- c("source","target","mor")
+  KSN_omnipath <- unique(KSN_omnipath)
+  
+  KSN_omnipath$comb <- paste(KSN_omnipath$source, KSN_omnipath$target, sep = "_")
+  dubbs <- KSN_omnipath[duplicated(KSN_omnipath$comb), "comb"]
+  
+  KSN_omnipath$mor <- ifelse(KSN_omnipath$comb %in% dubbs, -1, KSN_omnipath$mor)
+  KSN_omnipath <- unique(KSN_omnipath)
+  
+  KSN_omnipath <- KSN_omnipath[,-4]
+  
+  return(KSN_omnipath)
+}

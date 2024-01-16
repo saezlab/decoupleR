576cf86cc559ba76abf24a8b4cf57f35ce6f06a9
PauBadiaM <paubadiamompel@gmail.com>
2024-01-16 16:51:21 +0100

    Updated docs

--- R/utils-omnipath.R
+++ R/utils-omnipath.R
@@ -6,7 +6,7 @@
 #' factors (TFs) and their target genes. Each interaction is weighted by its
 #' mode of regulation (either positive or negative) and by its confidence level
 #'
-#' @param organism Which organism to use. Only human and mouse are available.
+#' @param organism Which organism to use. Only human, mouse and rat are available.
 #' @param levels List of confidence levels to return. Goes from A to D, A
 #' being the most confident and D being the less.
 #' @param weight_dict Dictionary of values to divide the mode of regulation
@@ -81,7 +81,7 @@ get_dorothea <- function(organism='human', levels=c('A', 'B', 'C'),
 #' factors (TFs) and their target genes. It is an expansion of DoRothEA.
 #' Each interaction is weighted by its mode of regulation (either positive or negative).
 #'
-#' @param organism Which organism to use. Only human and mouse are available.
+#' @param organism Which organism to use. Only human, mouse and rat are available.
 #' @param split_complexes Whether to split complexes into subunits. By default
 #' complexes are kept as they are.
 #' @param ... Ignored.
